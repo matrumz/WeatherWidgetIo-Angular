3fe9d9a81dd3a5064d208b59a0c2d9ef5ee13792
matrumz <matrumz@hotmail.com>
2018-07-24 18:50:10 -0400

    ng generate component weather
    ng g c weather --project weather-widget-io-angular

--- projects/weather-widget-io-angular/src/lib/weather-widget-io-angular.module.ts
+++ projects/weather-widget-io-angular/src/lib/weather-widget-io-angular.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { WeatherWidgetIoAngularComponent } from './weather-widget-io-angular.component';
+import { WeatherComponent } from './weather/weather.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [WeatherWidgetIoAngularComponent],
+  declarations: [WeatherWidgetIoAngularComponent, WeatherComponent],
   exports: [WeatherWidgetIoAngularComponent]
 })
 export class WeatherWidgetIoAngularModule { }
---
+++ projects/weather-widget-io-angular/src/lib/weather/weather.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'wwio-weather',
+  templateUrl: './weather.component.html',
+  styleUrls: ['./weather.component.css']
+})
+export class WeatherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
