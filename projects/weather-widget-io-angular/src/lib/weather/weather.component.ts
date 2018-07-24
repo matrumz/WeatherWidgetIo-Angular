5fc27a27f427ed13eb083c59d9955e9327394d84
matrumz <matrumz@hotmail.com>
2018-07-24 19:48:49 -0400

    WeatherComponent minimal widget demo works

--- projects/weather-widget-io-angular/src/lib/weather/weather.component.ts
+++ projects/weather-widget-io-angular/src/lib/weather/weather.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
-  selector: 'wwio-weather',
-  templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.less']
+    selector: 'wwio-weather',
+    templateUrl: './weather.component.html',
+    styleUrls: ['./weather.component.less']
 })
-export class WeatherComponent implements OnInit {
+export class WeatherComponent implements OnInit
+{
 
-  constructor() { }
+    constructor(
+        @Inject(DOCUMENT) private document: any // Using 'Document' type causes build error: "Metadata collected contains an error that will be reported at runtime: Could not resolve type Document."
+        // @Input('url') public forecast7Url: string
+    ) {
+        // this.forecast7Url = "https://forecast7.com/en/43d66n70d26/portland/?unit=us";
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit()
+    {
+        this.wwioInit();
+    }
+
+    private wwioInit(): void
+    {
+        let d = this.document;
+        const s: string = "script";
+        const id: string = "weatherwidget-io-js";
+
+        let js;
+        let fjs = d.getElementsByTagName(s)[0];
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = 'https://weatherwidget.io/js/widget.min.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }
 
 }
