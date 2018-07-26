7bca2f9732b94b6ecaadb74410475b6b19247e27
matrumz <matrumz@hotmail.com>
2018-07-26 10:21:33 -0400

    WeatherComponent @Inject and @Input working
    @ Inject must be in constructor params
    @ Input properties declared like the rest of my class properties

--- projects/weather-widget-io-angular/src/lib/weather/weather.component.ts
+++ projects/weather-widget-io-angular/src/lib/weather/weather.component.ts
@@ -1,24 +1,20 @@
-import { Component, OnInit, Inject } from '@angular/core';
+import { Component, OnInit, Inject, Input } from '@angular/core';
 import { DOCUMENT } from '@angular/common';
 import { IWeatherWidgetIoWidget } from '../models/weather-widget-io-widget';
 
 @Component({
     selector: 'wwio-weather',
     templateUrl: './weather.component.html',
-    styleUrls: ['./weather.component.less'],
-    inputs: [
-        "widget.href: url"
-    ]
+    styleUrls: ['./weather.component.less']
 })
-export class WeatherComponent implements OnInit
+export class WeatherComponent implements OnInit, IWeatherWidgetIoWidget
 {
 
+    @Input("url") public href: string;
+
     constructor(
-        @Inject(DOCUMENT) private document: any, // Using 'Document' type causes build error: "Metadata collected contains an error that will be reported at runtime: Could not resolve type Document."
-    )
-    {
-        this.widget = {} as IWeatherWidgetIoWidget;
-    }
+        @Inject(DOCUMENT) private document: any // Using 'Document' type causes build error: "Metadata collected contains an error that will be reported at runtime: Could not resolve type Document."
+    ) {}
 
     ngOnInit()
     {
@@ -40,7 +36,4 @@ export class WeatherComponent implements OnInit
             fjs.parentNode.insertBefore(js, fjs);
         }
     }
-
-    public widget: IWeatherWidgetIoWidget;
-
 }
